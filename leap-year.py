0abdd513dff98a95aa04f65be76a0c6a2a732820
Hilal <hilalsyn@gmail.com>
2021-11-23 11:32:43 -0500

    leapyear file added

---
+++ leap-year.py
@@ -0,0 +1,14 @@
+# To get year (integer input) from the user
+# This is my first explanation
+year = int(input("Enter a year: "))
+​
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("{0} is a leap year".format(year))
+        else:
+            print("{0} is not a leap year".format(year))
+    else:
+        print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
